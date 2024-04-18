5e79957a8d055b26602b31182915ab25b525e84a
VRUJESH HM <Vrujesh.HM@foreignadmits.com>
2024-04-18 12:19:28 +0530

    DEV

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,23 @@
-export default function Home(){
-    return <>
-    <div>Hello Home</div>
-    </>
+import Link from "next/link";
+import { cookies } from 'next/headers'
+export default async function Home() {
+    const cookieStore = cookies()
+    const token: any = cookieStore.get("token")
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            Hello world This is a Home page
+            {!token && !token?.value ? (
+                <>
+                    <Link href="/login">Visit login page</Link>
+
+                    <Link href="/signup">Visit Signup page</Link>
+                </>
+
+            ) : (<div>
+                <p>Welcome! You are logged in.</p>
+                <Link href="/profile">Profile Page Access</Link>
+
+            </div>)}
+        </main>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 import Link from "next/link";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello world This is a sample page 
+      Hello world This is a sample page
       <Link href="/login">Visit login page</Link>
 
       <Link href="/signup">Visit Signup page</Link>
