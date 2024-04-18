31e8e21cbcee032ecf3336bdbb9c966844aa8e24
VRUJESH HM <Vrujesh.HM@foreignadmits.com>
2024-04-18 11:26:02 +0530

    remove.env

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,9 +1,9 @@
 import { connect } from "@/dbConfig/dbConfig";
-import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+const User = require("@/models/userModel")
 connect()
 
 export async function POST(request: NextRequest) {
@@ -21,33 +21,34 @@ export async function POST(request: NextRequest) {
         console.log("user exists");
 
 
-        //check if password is correct
-        const validPassword = await bcryptjs.compare(password, user.password)
-        if (!validPassword) {
-            return NextResponse.json({ error: "Invalid password" }, { status: 400 })
-        }
-        console.log(user);
+        // //check if password is correct
+        // const validPassword = await bcryptjs.compare(password, user.password)
+        // if (!validPassword) {
+        //     return NextResponse.json({ error: "Invalid password" }, { status: 400 })
+        // }
+        // console.log(user);
 
-        //create token data
-        const tokenData = {
-            id: user._id,
-            username: user.username,
-            email: user.email
-        }
-        //create token
-        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1d" })
+        // //create token data
+        // const tokenData = {
+        //     id: user._id,
+        //     username: user.username,
+        //     email: user.email
+        // }
+        // //create token
+        // const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1d" })
 
         const response = NextResponse.json({
             message: "Login successful",
             success: true,
         })
-        response.cookies.set("token", token, {
-            httpOnly: true,
+        // response.cookies.set("token", token, {
+        //     httpOnly: true,
 
-        })
+        // })
         return response;
 
     } catch (error: any) {
+        console.error(error.message)
         return NextResponse.json({ error: error.message }, { status: 500 })
     }
 }
